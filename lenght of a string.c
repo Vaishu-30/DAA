cedce6ecea11e1652aa9d54ed3d9155e3d68d8c6
Vaishu-30 <113538323+Vaishu-30@users.noreply.github.com>
2023-08-17 10:42:53 +0530

    Add files via upload

---
+++ lenght of a string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char s[]="running";
+	int i;
+	for(i=0;s[i]!='\0';++i)
+	{
+		printf("lenght of the string: %d",i);
+	}
+	return 0;
+}
---
+++ reverse a string.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str1[50],temp;
+	int i=0,j=0;
+	printf("enter a string to be reversed:");
+	scanf("%s",str1);
+	j=strlen(str1)-1;
+	while(i<j)
+	{
+		temp=str1[j];
+		str1[j]=str1[i];
+		str1[i]=temp;
+		i++;
+		j--;
+	}
+	printf("the reversed of the string:%s",str1);
+	return 0;
+}
